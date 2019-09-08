fb537189e6505a880dea57c3449289f8a847c906
Eli <elik051@hotmail.com>
2019-09-08 17:16:51 +0300

    color changing project

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-// console.log(document)
+
+for(i=0;i<6;i++){
+    let squaries=document.createElement("div")
+    squaries.style.backgroundColor=getRandomColor();
+    squaries.setAttribute("id","box"+i)
+    squaries.style.margin="1px"
+    squaries.style.height="50px"
+    squaries.style.width="50px"
+    squaries.style.border="1px solid black"
+    squaries.style.display="inline-block"
+    squaries.setAttribute("class", "squary")
+    document.body.appendChild(squaries)
+    squaries.onmouseenter=function(){
+        squaries.style.backgroundColor=getRandomColor();
+    }
+    }
+    
+    
+    
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];//random makes 0-1//floor rounds it to natural num
+        }
+        return color;
+      }
+    
